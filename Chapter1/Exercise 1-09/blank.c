c49ab9660d2e70e3da954fcb6e35951cf02b3f33
tanyeun <tanyeun@users.noreply.github.com>
2017-02-08 16:35:30 +0800

    Update blank.c

--- Chapter1/Exercise 1-09/blank.c
+++ Chapter1/Exercise 1-09/blank.c
@@ -1,26 +1,22 @@
-/**
- * Author: Jeremy Yu <ccpalettes@gmail.com>
- * 
- * Solution for Exercise 1-9, Chapter1.
- */
-
 #include <stdio.h>
 
-int main(void)
+int main()
 {
-    int c, blank_recieved;
-    blank_recieved = 0;
-    printf("Input some characters, when you finishes, press Ctrl+D.\n");
-    while ((c = getchar()) != EOF)
-        if (c == ' ') {
-            if (!blank_recieved) {
-                blank_recieved = 1;
-                putchar(c);
-            }
-        } else {
-            blank_recieved = 0;
-            putchar(c);
-        }
+  int c;
+  int has_blank = 0;
+  while( (c = getchar() ) != EOF )
+  {
+    if ( c == ' ' )
+	{
+	  if( has_blank == 0 )
+	  {
+	    has_blank = 1;
+	  }else
+	    continue;
+	}else
+	  has_blank = 0;
+	putchar(c);
+  }
 
-    return 0;
+  return 0;
 }
